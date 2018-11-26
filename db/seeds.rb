adef2534cfe388b23d48c010a67f849b6c0000f5
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-26 19:56:34 +0000

    Commented out stupid test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+artists = Artist.create([{name: "Snoop Dogg"}, {name: "Dr. Dre"}, {name: "Eminem"}, {name: "Eazy E."}])
+songs = Song.create([{title: "Drop It Like It's Hot", artist_id: 1}, {title: "The Next Episode", artist_id: 2}, {title: "Lose Yourself", artist_id: 3}, {title: "We Want Eazy", artist_id: 4}])
